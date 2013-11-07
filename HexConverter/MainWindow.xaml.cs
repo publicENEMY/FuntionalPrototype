f1d6750648502ca4fd006b3c5f9457aac865a97b
publicENEMY <bukan.ijam@gmail.com>
2013-11-07 09:07:14 +0800

    Added template

--- HexConverter/MainWindow.xaml.cs
+++ HexConverter/MainWindow.xaml.cs
@@ -24,5 +24,39 @@ namespace HexConverter
 		{
 			InitializeComponent();
 		}
+
+		private void convertFromInt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private static string ConvertHexByteToHexString(string hexByte)
+		{
+			string s = null;
+			return s;
+		}
+
+		private static string ConvertHexStringToHexByte(string hexString)
+		{
+			string s = null;
+			return s;
+		}
+
+		private static int ConvertHexStringToInt(string hex)
+		{
+			return Convert.ToInt32(hex, 16);
+		}
+	
+		private static string ConvertIntToHexString(int intValue)
+		{
+			string s = null;
+			return s;
+		}
+
+		private static byte[] ConvertIntToByteArray(int intValue)
+		{
+			byte[] byteArray = new byte[3];
+			return byteArray;
+		}
 	}
 }
