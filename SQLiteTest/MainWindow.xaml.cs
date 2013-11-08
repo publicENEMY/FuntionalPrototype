395ecbc5e877a0015d1823c9b46836ceab1738af
publicENEMY <publicENEMY@github.com>
2013-11-08 18:07:13 +0800

    Working sqlite database

--- SQLiteTest/MainWindow.xaml.cs
+++ SQLiteTest/MainWindow.xaml.cs
@@ -56,38 +56,120 @@ namespace SQLiteTest
 			private set { _messages = value; }
 		}
 
-		private SQLiteConnection _db;
+		/*
+		 * var db = new SQLiteConnection("sqllite.db")
+		 * db.CreateTable<SyncRecord> ();
+		 * db.Insert (new SyncRecord () { SyncDate = DateTime.UtcNow });
+		 * var query = db.Table<SyncRecord> ().Where(your lambda to filter);
+		 */
+		private string dbPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "MyDatabase.sqlite");
 
 		private void createDb_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-			Logger.Info("Creating database");
-			var dbPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Stocks.db");
 			if (File.Exists(dbPath))
 				Logger.Info("File " + dbPath + " already exists");
 			try
 			{
-				_db = new SQLiteConnection(dbPath);
+				using (var m_dbConnection = new SQLite.SQLiteConnection(dbPath))
+				{
+				}
+				Logger.Info("Database created/connected");
 			}
 			catch (System.Exception ex)
 			{
 				Logger.Error(ex.Message);
 			}
 			if (File.Exists(dbPath))
-				Logger.Info("File " + dbPath + " exists");
+				Logger.Info("Verified " + dbPath + " exists");
 			else
-				Logger.Info("File " + dbPath + " doesnt exists");
+				Logger.Error("Verified " + dbPath + " doesnt exists");
 		}
 
-		private void connectDb_Click(object sender, RoutedEventArgs e)
+		public class highscore
 		{
+			[MaxLength(20)]
+			public string name { get; set; }
+			public int score { get; set; }
 		}
 
 		private void createTb_Click(object sender, RoutedEventArgs e)
 		{
 			// list available table
+
 			// create table
+			using (var m_dbConnection = new SQLite.SQLiteConnection(dbPath))
+			{
+				try
+				{
+					m_dbConnection.CreateTable<highscore>();
+					Logger.Info("Table created");
+				}
+				catch (System.Exception ex)
+				{
+					Logger.Error(ex.Message);
+				}
+			}
+
 			// check table exist
-			// 
+
+			/*
+			 * var db = new SQLiteConnection("sqllite.db")
+			 * db.CreateTable<SyncRecord> ();
+			 * db.Insert (new SyncRecord () { SyncDate = DateTime.UtcNow });
+			 * var query = db.Table<SyncRecord> ().Where(your lambda to filter);
+			 */
+		}
+
+		private void fillTb_Click(object sender, RoutedEventArgs e)
+		{
+			using (var m_dbConnection = new SQLite.SQLiteConnection(dbPath))
+			{
+				try
+				{
+					m_dbConnection.Insert(new highscore()
+					{
+						name = "Me",
+						score = 9001
+					});
+					m_dbConnection.Insert(new highscore()
+					{
+						name = "Me",
+						score = 3000
+					});
+					m_dbConnection.Insert(new highscore()
+					{
+						name = "Myself",
+						score = 6000
+					});
+					m_dbConnection.Insert(new highscore()
+					{
+						name = "And I",
+						score = 9001
+					});
+
+					Logger.Info("Filled table");
+				}
+				catch (System.Exception ex)
+				{
+					Logger.Error(ex.Message);
+				}
+			}
+		}
+
+		private void queryDb_Click(object sender, RoutedEventArgs e)
+		{
+			//string sql = "select * from highscores order by score desc";
+			//SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+			//SQLiteDataReader reader = command.ExecuteReader();
+			//while (reader.Read())
+			//	Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+
+			using (var m_dbConnection = new SQLite.SQLiteConnection(dbPath))
+			{
+				//var query = m_dbConnection.Table<highscore> ().Where(your lambda to filter);
+
+				//var query2 =m_dbConnection.Query<highscore> ("",);
+			}
 		}
 
 	}
