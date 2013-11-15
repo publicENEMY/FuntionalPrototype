c3098d5a2ccf984ed9e08f741ff68a40e7158e55
publicENEMY <publicENEMY@github.com>
2013-11-15 18:02:29 +0800

    Progress bar working

--- ProgressBarAsync/MainWindow.xaml.cs
+++ ProgressBarAsync/MainWindow.xaml.cs
@@ -25,10 +25,53 @@ namespace ProgressBarAsync
 			InitializeComponent();
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+		private async void Button_Click(object sender, RoutedEventArgs e)
 		{
 			var newWindow = new Progress();
 			newWindow.Show();
+
+			// The Progress<T> constructor captures our UI context,
+			//  so the lambda will be run on the UI thread.
+			var progress = new Progress<int>(percent =>
+			{
+				textBox1.Text = percent + "%";
+				newWindow.connectionProgress.Value = percent;
+			});
+
+			// DoProcessing does most of its work on the thread pool.
+			await DoProcessingOnThreadPoolAsync(progress);
 		}
+
+		private async Task DoProcessingOnThreadPoolAsync(IProgress<int> progress)
+		{
+			int timer = 0;
+
+			do
+			{
+				await Task.Delay(10);
+				if (progress != null)
+					progress.Report(timer);
+				timer += 1;
+			} while (timer<=100);
+		}
+
+		/*
+		public async Task DownloadFileAsync(string fileName, IProgress<int> progress)
+{
+  using (var fileStream = ...) // Open local file for writing
+  using (var ftpStream = ...) // Open FTP stream
+  {
+    while (true)
+    {
+      var bytesRead = await ftpStream.ReadAsync(...);
+      if (bytesRead == 0)
+        return;
+      await fileStream.WriteAsync(...);
+      if (progress != null)
+        progress.Report(bytesRead);
+    }
+  }
+}*/
+
 	}
 }
